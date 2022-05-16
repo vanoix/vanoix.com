a05a15a761ddf43ee2bce5e84be9f108f16c6cd4
Alexandre Balmes <alexandre@vanoix.com>
2022-05-16 21:35:04 +0200

    Add new template with new content

--- assets/front/app.js
+++ assets/front/app.js
@@ -1,52 +1,27 @@
 require('./app.scss');
 
-const $ = require('jquery');
-require('bootstrap');
+let main = document.getElementById('main-menu');
+let list = document.getElementById('responsive-list');
+let closeButton = document.getElementById('close-button');
 
-$(document).ready(function() {
-  const json = require('./data/data.json');
-  const whoami = json.jobs;
+let closeAction = ['opacity-0', 'scale-95', 'hidden'];
+let openAction = ['opacity-100', 'scale-100', 'shown'];
 
-  $.each(whoami, function(index, jobs) {
-    $('#whoami').append("<option name=" + index + ">" + jobs.value + "</option>");
-  });
+function hide() {
+  if(list.classList.contains('shown')) {
+    list.classList.remove(...openAction);
+    list.classList.add(...closeAction);
+  }
+}
 
-  $('#whoami').change(function() {
-    var current = $(this).children("option:selected").attr("name");
+function show() {
+  if(list.classList.contains('hidden')) {
+    list.classList.remove(...closeAction);
+    list.classList.add(...openAction);
+  }
+}
 
-    if (null == current) {
-      $('#incentive-text').empty();
-      $('#want').find('option:not(:first)').remove().end();
-      return;
-    }
-
-    var choices = whoami[current].choices;
-
-    $('#want').find('option:not(:first)').remove().end();
-    $('#incentive-text').empty();
-
-    $.each(choices, function(current, choice) {
-      var help = json.choices[choice];
-      $('#want').append("<option name=" + help.key +">" + help.value + "</option>");
-    });
-  });
-
-  $('#want').change(function () {
-    var whoami = $('#whoami').children("option:selected").attr("name");
-    var want = $(this).children("option:selected").attr("name");
-    var text = whoami + "-" + want;
-    var content = json.text;
-
-    if (null == want) {
-      $('#incentive-text').empty();
-      return;
-    }
-
-    if ('formation' === want) {
-      $('#incentive-text').html("<p>" + content[text] +" Plus d'informations sur <a href=\"https://formation.vanoix.com\">notre site dédié !</a></p>");
-      return;
-    }
-
-    $('#incentive-text').html("<p>" + content[text] +" <a href=\"mailto:hello@vanoix.com?subject=[Prise de contact]\">Contactez-nous !</a></p>");
-  });
+document.addEventListener("DOMContentLoaded", function(event) {
+  main.onclick = function() { show(); }
+  closeButton.onclick = function() { hide(); }
 });
